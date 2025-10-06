042c6fcfc94f0415f4dc826ec1ae3a31ec6527d6
Robert Pallesen <Robert.pallesen13@gmail.com>
2025-10-06 13:51:24 +0200

    add downloadPdf + test

--- src/main/java/org/example/app/Main.java
+++ src/main/java/org/example/app/Main.java
@@ -1,14 +1,24 @@
 package org.example.app;
 
-
 import org.example.service.ConverterService;
+
+import java.net.URL;
 
 public class Main {
     public static void main(String[] args) {
 
-
         ConverterService converter = new ConverterService();
-        converter.convertExcelToCSV();
 
+        try {
+            URL url = new URL("http://cdn12.a1.net/m/resources/media/pdf/A1-Umwelterkl-rung-2016-2017.pdf");
+            String excelFilePath = "C:\\Specialisterne - Opgaver\\Uge 4\\data\\GRI_2017_2020_TEST.xlsx";
+            String fileName = "testFile";
+            String pathToSave = "Downloads";
+
+            converter.getURLFromExcel(excelFilePath);
+            converter.downloadPDF(url, fileName + ".pdf", pathToSave);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/example/service/ConverterService.java
+++ src/main/java/org/example/service/ConverterService.java
@@ -1,29 +1,61 @@
 package org.example.service;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
 
 public class ConverterService {
 
+    // TODO: Check for URL status code/is accessible
+    // TODO: Check if URL is a valid PDF
+    // TODO: Check if URL is not too large
+    // TODO: Check if URL is corrupted
+    // TODO: Check if URL is not empty
+    // TODO: Check if PDF is a duplicate
+    // TODO: Check if PDF is not encrypted
+    // TODO: Parser to get URL from excel file
 
-    public void convertExcelToCSV() {
-
-        String csvPath = "C:\\Specialisterne - Opgaver\\Uge 4\\data\\GRI_2017_2020_TEST.xlsx";
-
-
-        try (BufferedReader br = new BufferedReader(new FileReader(csvPath))) {
-
-            System.out.println(br);
-            System.out.println(br.read());
-            System.out.println(br.readLine().getClass().getName());
+    public void getURLFromExcel(String filePath) {
+        // Use Apache POI to read the Excel file and extract URLs from the "Pdf_URL" column
+        // Return the URLs as a list of strings
+        // For each URL, call the downloadPDF method to download the PDF
+        System.out.println(filePath);
+        System.out.println("WIP");
+    }
 
 
+    // Download PDF from URL and save it to specified path with the specified file name
+    public void downloadPDF(URL urlStr, String fileName, String targetPath) {
 
-        } catch (IOException e) {
+        // TODO: Threading: Make this method take an array of URLs and download them in parallel using threads
+        // TODO: If the targetPath does not exist, create it
+        // TODO: If the file exists, ask the user if they want to overwrite it
+        // TODO: If the file already exists, append a number to the file name
+
+        try {
+            String downloadsPath = System.getProperty("user.home") + "\\" + targetPath + "\\" + fileName;
+
+            // open the connection to the url
+            InputStream in = urlStr.openStream();
+            FileOutputStream fos = new FileOutputStream(downloadsPath);
+
+            // read the data from the url and write it to the file
+            System.out.println("Reading data from URL...");
+            int length;
+            byte[] buffer = new byte[1024];
+            while ((length = in.read(buffer)) != -1) {
+                fos.write(buffer, 0, length);
+            }
+
+            // close the streams
+            fos.close();
+            in.close();
+
+            System.out.println("PDF downloaded successfully!");
+            System.out.println("Downloaded at: ");
+            System.out.println(downloadsPath);
+
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 }
-
