18bb330b97c29943f5e8fc7253ccfa057dbca0bf
Robert Pallesen <Robert.pallesen13@gmail.com>
2025-10-06 15:00:29 +0200

    updated test

--- src/main/java/org/example/service/ConverterService.java
+++ src/main/java/org/example/service/ConverterService.java
@@ -1,7 +1,17 @@
 package org.example.service;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class ConverterService {
 
@@ -13,24 +23,61 @@ public class ConverterService {
     // TODO: Check if PDF is a duplicate
     // TODO: Check if PDF is not encrypted
     // TODO: Parser to get URL from excel file
+    // TODO: Limit threads
 
-    public void getURLFromExcel(String filePath) {
-        // Use Apache POI to read the Excel file and extract URLs from the "Pdf_URL" column
+    public List<String> getURLFromExcel(String excelFilePath) throws IOException {
+        // Read the Excel file and extract URLs from the "Pdf_URL" column
         // Return the URLs as a list of strings
-        // For each URL, call the downloadPDF method to download the PDF
-        System.out.println(filePath);
-        System.out.println("WIP");
+        // TODO: For each URL, call the downloadPDF method to download the PDF (threading)
+        // TODO: Add option for using the alternative URL (Report Html Address)
+
+        List<String> pdfUrls = new java.util.ArrayList<>();
+
+        // Open the Excel file
+        FileInputStream file = new FileInputStream(excelFilePath);
+        Workbook workbook = new XSSFWorkbook(file);
+
+        // Get the first sheet
+        Sheet sheet = workbook.getSheetAt(0);
+
+        // iterate through the rows in the sheet, starting from row 1 (row 0 is the header)
+        for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) {
+
+            // get the current row and the header row
+            Row currentRow = sheet.getRow(i);
+            Row headerRow = sheet.getRow(0);
+
+            // iterate through the cells in the header row
+            for (int col = 0; col < headerRow.getLastCellNum(); col++) {
+
+                // get the cell in the header row
+                Cell cell = headerRow.getCell(col);
+
+                if (cell != null && "Pdf_URL".equals(cell.getStringCellValue())) {
+                    // get the index of the column where the header is "Pdf_URL"
+                    int pdf_index = cell.getColumnIndex();
+
+                    // get the cell in the current row at the index of the "Pdf_URL" column
+                    Cell pdfLink = currentRow.getCell(pdf_index);
+
+                    // print the value of the cell
+                    pdfUrls.add(pdfLink.getStringCellValue());
+                }
+            }
+        }
+
+        return pdfUrls;
     }
 
 
     // Download PDF from URL and save it to specified path with the specified file name
-    public void downloadPDF(URL urlStr, String fileName, String targetPath) {
+    public boolean downloadPDF(URL urlStr, String fileName, String targetPath) {
 
         // TODO: Threading: Make this method take an array of URLs and download them in parallel using threads
         // TODO: If the targetPath does not exist, create it
         // TODO: If the file exists, ask the user if they want to overwrite it
         // TODO: If the file already exists, append a number to the file name
-
+        System.out.println("Opening connection to download PDF...");
         try {
             String downloadsPath = System.getProperty("user.home") + "\\" + targetPath + "\\" + fileName;
 
@@ -53,9 +100,33 @@ public class ConverterService {
             System.out.println("PDF downloaded successfully!");
             System.out.println("Downloaded at: ");
             System.out.println(downloadsPath);
-
+            return true;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
+
+    public Path downloadPDF2(URL url, String fileName, Path targetDir) throws IOException {
+        Files.createDirectories(targetDir);
+        Path out = targetDir.resolve(fileName);
+
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setConnectTimeout(10_000);
+        con.setReadTimeout(20_000);
+        con.setInstanceFollowRedirects(true);
+        con.setRequestProperty("Accept", "application/pdf,*/*;q=0.9");
+        con.connect();
+
+        int code = con.getResponseCode();
+        if (code != HttpURLConnection.HTTP_OK) {
+            throw new IOException("HTTP " + code + " for " + url);
+        }
+
+        try (InputStream in = con.getInputStream();
+             OutputStream fos = Files.newOutputStream(out)) {
+            in.transferTo(fos);
+        }
+        return out;
+    }
+
 }
--- src/test/java/org/example/service/ConverterServiceTest.java
+++ src/test/java/org/example/service/ConverterServiceTest.java
@@ -1,11 +1,6 @@
 package org.example.service;
 
 import junit.framework.TestCase;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
 import java.net.URL;
@@ -13,7 +8,7 @@ import java.util.*;
 
 public class ConverterServiceTest extends TestCase {
 
-    private final String dataPath = "C:\\Specialisterne - Opgaver\\Uge 4\\data\\GRI_2017_2020_TEST.xlsx";
+    private final ConverterService service = new ConverterService();
 
     public void setUp() throws Exception {
         System.out.println("Global setup");
@@ -29,73 +24,25 @@ public class ConverterServiceTest extends TestCase {
     public void testGetURLFromExcel() throws IOException {
 
         // Open the Excel file
-        FileInputStream file = new FileInputStream(dataPath);
-        Workbook workbook = new XSSFWorkbook(file);
+        String dataPath = "C:\\Specialisterne - Opgaver\\Uge 4\\data\\GRI_2017_2020_TEST.xlsx";
+        List<String> pdfUrls = service.getURLFromExcel(dataPath);
 
-        // Get the first sheet
-        Sheet sheet = workbook.getSheetAt(0);
-
-        // iterate through the rows in the sheet, starting from row 1 (row 0 is the header)
-        for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) {
-
-            // get the current row and the header row
-            Row currentRow = sheet.getRow(i);
-            Row headerRow = sheet.getRow(0);
-
-            // iterate through the cells in the header row
-            for (int col = 0; col < headerRow.getLastCellNum(); col++) {
-
-                // get the cell in the header row
-                Cell cell = headerRow.getCell(col);
-
-                if (cell != null && "Pdf_URL".equals(cell.getStringCellValue())) {
-                    // get the index of the column where the header is "Pdf_URL"
-                    int pdf_index = cell.getColumnIndex();
-
-                    // get the cell in the current row at the index of the "Pdf_URL" column
-                    Cell pdfLink = currentRow.getCell(pdf_index);
-
-                    // print the value of the cell
-                    System.out.println("PDF Link for row " + i + ": " + pdfLink.getStringCellValue());
-                }
-            }
-        }
+        assertEquals(4, pdfUrls.size());
     }
 
-    public void testDownloadPDF() {
-        System.out.println("Opening connection to download PDF...");
-        try {
-            // the url of the pdf to be downloaded
-            URL url = new URL("http://cdn12.a1.net/m/resources/media/pdf/A1-Umwelterkl-rung-2016-2017.pdf");
+    public void testDownloadPDF() throws Exception {
 
-            // name of the downloaded pdf
-            String fileName = "MyDownloadedFile.pdf";
+        // the url of the pdf to be downloaded
+        URL url = new URL("http://cdn12.a1.net/m/resources/media/pdf/A1-Umwelterkl-rung-2016-2017.pdf");
 
-            // download path
-            String downloadsPath = System.getProperty("user.home") + "\\Downloads\\" + fileName;
+        // name of the downloaded pdf
+        String fileName = "MyDownloadedFile.pdf";
 
-            // open the connection to the url
-            InputStream in = url.openStream();
-            FileOutputStream fos = new FileOutputStream(downloadsPath);
+        // target path to save the downloaded pdf
+        String targetPath = "Downloads";
 
-            // read the data from the url and write it to the file
-            System.out.println("Reading data from URL...");
-            int length;
-            byte[] buffer = new byte[1024];
-            while ((length = in.read(buffer)) != -1) {
-                fos.write(buffer, 0, length);
-            }
-
-            // close the streams
-            fos.close();
-            in.close();
-
-            System.out.println("PDF downloaded successfully!");
-            System.out.println("File path: " + downloadsPath);
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        boolean isOk = service.downloadPDF(url, fileName, targetPath);
+        assertTrue(isOk);
     }
 }
 
